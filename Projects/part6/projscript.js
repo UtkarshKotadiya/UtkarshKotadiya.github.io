491619d06e6e9122978603baa8d8303dc5837deb
UtkarshKotadiya <utkarshkotadiya123@gmail.com>
2024-10-13 18:31:08 -0400

    10/13

--- Projects/part6/projscript.js
+++ Projects/part6/projscript.js
@@ -22,9 +22,76 @@ const getProducts = async() => {
 const showProducts = async () => {
     const products = await getProducts();
 
+    // Rings
     products.rings.forEach((item) => {
         const section = document.createElement("section");
         document.getElementById("ring-products").append(section);
+
+        const img = document.createElement("img");
+        img.src = "proj_imgs/products/" + item.img_name;
+        section.appendChild(img);
+
+        const description = document.createElement("h5");
+        description.textContent = item.description;
+        section.appendChild(description);
+
+        const price = document.createElement("h5");
+        price.textContent = `$${item.price}`;
+        section.appendChild(price);
+
+        const details = document.createElement("div");
+        details.textContent = item.details.join(", ");
+        section.appendChild(details);
+    });
+
+    // Necklaces
+    products.necklaces.forEach((item) => {
+        const section = document.createElement("section");
+        document.getElementById("necklace-products").append(section);
+
+        const img = document.createElement("img");
+        img.src = "proj_imgs/products/" + item.img_name;
+        section.appendChild(img);
+
+        const description = document.createElement("h5");
+        description.textContent = item.description;
+        section.appendChild(description);
+
+        const price = document.createElement("h5");
+        price.textContent = `$${item.price}`;
+        section.appendChild(price);
+
+        const details = document.createElement("div");
+        details.textContent = item.details.join(", ");
+        section.appendChild(details);
+    });
+
+    // Bracelets
+    products.bracelets.forEach((item) => {
+        const section = document.createElement("section");
+        document.getElementById("bracelet-products").append(section);
+
+        const img = document.createElement("img");
+        img.src = "proj_imgs/products/" + item.img_name;
+        section.appendChild(img);
+
+        const description = document.createElement("h5");
+        description.textContent = item.description;
+        section.appendChild(description);
+
+        const price = document.createElement("h5");
+        price.textContent = `$${item.price}`;
+        section.appendChild(price);
+
+        const details = document.createElement("div");
+        details.textContent = item.details.join(", ");
+        section.appendChild(details);
+    });
+
+    // Watches
+    products.watches.forEach((item) => {
+        const section = document.createElement("section");
+        document.getElementById("watch-products").append(section);
 
         const img = document.createElement("img");
         img.src = "proj_imgs/products/" + item.img_name;
